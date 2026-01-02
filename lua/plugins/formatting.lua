aec68bdb059cba03293c34b467b61b4e19482d26
FloofyPlasma <github@floofyplasma.com>
2026-01-02 12:02:06 -0500

    fix: remove deprecated format warning

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -4,21 +4,9 @@ return {
 		opts = {
 			formatters_by_ft = {
 				lua = { "stylua" },
-				-- Add more formatters as needed
-				-- python = { "isort", "black" },
-				-- javascript = { "prettierd", "prettier", stop_after_first = true },
+				c = { "clang-format" },
+				cpp = { "clang-format" },
 			},
-			format_on_save = function(bufnr)
-				-- Disable for specific filetypes if needed
-				local disable_filetypes = {}
-				if disable_filetypes[vim.bo[bufnr].filetype] then
-					return nil
-				end
-				return {
-					timeout_ms = 500,
-					lsp_fallback = true,
-				}
-			end,
 		},
 	},
 }
